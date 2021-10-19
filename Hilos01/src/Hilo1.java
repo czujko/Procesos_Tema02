419c26cd90f204e4006d2201b5359931e002d69e
czujko <n.dynmate@protonmail.com>
2021-10-19 13:43:39 +0200

    en clase

--- Hilos01/src/Hilo1.java
+++ Hilos01/src/Hilo1.java
@@ -1,18 +1,19 @@
-public class Hilo1 extends Thread{
-	public Hilo1(String nombre) {
-		super(nombre);
-		System.out.println("CREANDO " + getName());
-		System.out.println("constructor: " + getState());
-	}
-	public void run() {
-		if(getName().equals("Hilo2")){
-			try {
-				sleep(2000);
-			}catch (InterruptedException ie){
-				ie.printStackTrace();
-			}
-		}
-		System.out.println("Ejecutando HILO:" + getName() + " con Id " + getId());
-		System.out.println(getState());
-	}
+public class Hilo1 extends Thread {
+    public Hilo1(String nombre) {
+        super(nombre);
+        System.out.println("CREANDO " + getName());
+        System.out.println("constructor: " + getState());
+    }
+
+    public void run() {
+        if (getName().equals("Hilo2")) {
+            try {
+                sleep(2000);
+            } catch (InterruptedException ie) {
+                ie.printStackTrace();
+            }
+        }
+        System.out.println("Ejecutando HILO:" + getName() + " con Id " + getId());
+        System.out.println(getState());
+    }
 }
--- Hilos01/src/UsaHilo1.java
+++ Hilos01/src/UsaHilo1.java
@@ -1,16 +1,16 @@
 public class UsaHilo1 {
-    public static void main(String[] args){
-        Hilo1[] hilos=new Hilo1[5];
-        for(int n=0; n<5; n++) {
+    public static void main(String[] args) {
+        Hilo1[] hilos = new Hilo1[5];
+        for (int n = 0; n < 5; n++) {
             hilos[n] = new Hilo1("Hilo" + n);
-            if(n<3) {
+            if (n < 3) {
                 hilos[n].setPriority(Thread.MIN_PRIORITY);
-            }else{
+            } else {
                 hilos[n].setPriority(Thread.MAX_PRIORITY);
             }
             hilos[n].start();
         }
-        for(int n=0; n<5; n++) {
+        for (int n = 0; n < 5; n++) {
             System.out.println("Estado desde UsaHilo1:" + hilos[n].getState());
         }
     }
